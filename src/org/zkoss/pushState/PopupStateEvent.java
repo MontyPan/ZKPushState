8bd3a4fdff8fa316fcddf7fcb6a6547be4acbb9e
MontyPan <montypan@potix.com>
2012-12-10 19:09:35 +0800

    Change `state` generic type.

--- src/org/zkoss/pushState/PopupStateEvent.java
+++ src/org/zkoss/pushState/PopupStateEvent.java
@@ -7,17 +7,16 @@ import org.zkoss.zk.ui.event.Event;
 
 @SuppressWarnings("serial")
 public class PopupStateEvent extends Event {
-	private final Map<String, Object> _state;
+	private final Map<String, ?> _state;
 	
-	public PopupStateEvent(String name, Map<String, Object> state) {
+	public PopupStateEvent(String name, Map<String, ?> state) {
 		super(name);
 		this._state = state;
 	}
 	
-	public Map<String, Object> getState() {
+	public Map<String, ?> getState() {
 		return _state;
 	}
-
 
 	public static PopupStateEvent getPopupStateEvent(AuRequest request) {
 		return new PopupStateEvent(request.getCommand(), request.getData());
