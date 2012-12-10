a98da7ca3443a74bacbdc56b003c3ffb6380208d
MontyPan <montypan@potix.com>
2012-12-10 15:27:11 +0800

    Change `state` argument type of pust() and improve JavaDoc comment.

--- src/org/zkoss/pushState/PushState.java
+++ src/org/zkoss/pushState/PushState.java
@@ -2,17 +2,21 @@ package org.zkoss.pushState;
 
 import java.util.Map;
 
+import org.zkoss.json.JSONObject;
 import org.zkoss.json.JSONValue;
 import org.zkoss.zk.ui.util.Clients;
 
 public class PushState {
 	/**
 	 * A ZK wrapper of JS <code>history.pushState()</code>.
+	 * It will use {@link JSONValue#toJSONString(Object)} to generate JSON string.
+	 * @see JSONValue#toJSONString(Object)
+	 * @see JSONObject#toJSONString(Map)
 	 * @param state The state of new url.
 	 * @param title The browser title, but most browser does not implement.
 	 * @param url New url. 
 	 */
-	public static void push(Map<String, Object> state, String title, String url) {
+	public static void push(Map<String, ?> state, String title, String url) {
 		StringBuffer js = new StringBuffer("history.pushState(");
 		js.append(JSONValue.toJSONString(state));
 		js.append(",'");
